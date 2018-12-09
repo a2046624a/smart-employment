9a74088b0aee226c7c4a6a10c729794fc77b54cb
Cbsydhs <cbsydhs@live.com>
2018-12-09 14:44:42 +0800

    v0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <Header></Header>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBottom msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBottom from './components/NavBottom.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NavBottom,
+    Header
   }
 }
 </script>
@@ -23,6 +26,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    text-align: center;
 }
 </style>
